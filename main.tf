29b4a6f54926b6d164efba63f355d1145e09c564
Eshant99 <Eshant99@users.noreply.github.com>
2025-03-10 11:48:57 +0530

    Use our HCP Terraform organization

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ resource "random_pet" "sg" {}
 resource "aws_instance" "web" {
   ami                    = "ami-1234567890abcdef0"  # Replace with your AMI ID
   instance_type          = "t2.micro"
-  vpc_security_group_ids = [data.aws_security_group.existing_sg.id]  # Reference the existing SG
+ vpc_security_group_ids = ["sg-009e46413f6fd069e"]  # Reference the existing SG
 
   user_data = <<-EOF
               #!/bin/bash
@@ -40,12 +40,6 @@ resource "aws_instance" "web" {
               echo "Hello World" > /var/www/html/index.html
               systemctl restart apache2
               EOF
-}
-
-# Data source to reference an existing Security Group
-data "aws_security_group" "existing_sg" {
-  # Replace with the actual Security Group ID or name
-  id = "sg-009e46413f6fd069e"  # Example: Use your existing SG ID
 }
 
 output "web-address" {
